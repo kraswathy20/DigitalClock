f71ab61840d3688c81d99cd73bf5facad36fda3a
Aswathy k.r <aswathy19kr@gmail.com>
2024-11-28 00:11:54 +0530

    Digital clock using useEffect Hook

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import DigitalClock from './digitalClock'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     
+      <DigitalClock/>
+      
+      
+    </>
+  )
+}
+
+export default App
